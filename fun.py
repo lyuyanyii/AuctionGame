5feffb824b98f3f6e7e12e7c24a4f64664d2faba
hza <hza@megvii.com>
2016-06-25 14:09:30 +0800

    work for first price and second price

--- fun.py
+++ fun.py
@@ -6,9 +6,9 @@ from PublicKnowledge import PublicKnowledge
 class Policy:
     def __init__(self):
         self.params = []
-        self.inpdim = 1
         self.nonlinearity = lambda x:T.tanh(x)
         self.num_layers = 0
+        self.sigma = 0.01
         self.build_network()
 
     def build_network(self):
@@ -38,7 +38,10 @@ class Policy:
 
         def calc_prob(gaussian, var):
             mean = gaussian[:, 0]
-            sigma = T.exp( gaussian[:, 1] )
+            if self.sigma is not None:
+                sigma = self.sigma
+            else:
+                sigma = T.exp( gaussian[:, 1] )
 
             prob = 1/(sigma * np.pi**0.5) * T.exp(-(var - mean)**2/(2 * sigma**2))
             return prob
@@ -53,7 +56,7 @@ class Policy:
         self.forward = theano.function(inputs = [inp], outputs = gaussian)
 
         prob = calc_prob(gaussian, var)
-        loss = T.mean( T.log(prob) * (reward- reward.mean()) )
+        loss = -T.mean( T.log(prob) * (reward- reward.mean()) )
 
         updates = []
         for i in self.params:
@@ -68,7 +71,10 @@ class Policy:
         inp = inp.reshape(inp.shape[0], -1).astype(np.float32)
         gaussian = self.forward(inp)
         mean = gaussian[:, 0]
-        sigma = np.exp( gaussian[:, 0] )
+        if self.sigma is None: 
+            sigma = np.exp( gaussian[:, 0] )
+        else:
+            sigma = self.sigma
         sampled_var = (np.random.normal(size = mean.shape) * sigma + mean).astype('float32')
 
         self.last_inp = inp
@@ -112,7 +118,8 @@ class Player:
     def inform(self, message):
         reward = list( map(lambda a: self.utility(*a), zip(self.valuation.reshape(-1), message)) )
         if self.trainable:
-            self.policy.learning(reward)
+            loss = self.policy.learning(reward)
+            print(np.mean(reward), self.valuation[0], self.policy.last_var[0], loss)
         return reward
 
 class FakePlayer(Player):
@@ -134,7 +141,7 @@ class Mechanism:
         results = []
         for batches, acts in enumerate(actions):
             assert len(acts)!=1
-            t = np.argsort(acts)
+            t = np.argsort(-acts)
             results.append(  {
                 'bider': t[0],
                 'price': acts[t[1]]
@@ -158,9 +165,9 @@ def OneAgent():
     n = len(PublicKnowledge)
     players = [Player(0)] + [FakePlayer(i) for i in range(1, n)]
     gm = GameMaster(players)
-    for i in range(10):
+    for i in range(1000):
         gm.run(100)
-        print(players[0].policy.last_inp[0], players[0].policy.last_var[0])
+#        print(players[0].policy.last_inp[0], players[0].policy.last_var[0])
 
 if __name__ == '__main__':
     OneAgent()
